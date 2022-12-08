edcc5a269001cf448128954a66b0c31e86b9a620
RAPH <raphaelboutin70@gmail.com>
2022-12-08 12:10:50 +0100

    print list city

--- src/CityList.js
+++ src/CityList.js
@@ -1,36 +1,36 @@
-
 import { Component } from "react";
-class CityList extends Component{
+class CityList extends Component {
   ApiKey = "7886a12c53604b2668a08582a04795afcc9375b0";
 
-  constructor() {
-    super()
-  }
-  componentWillMount() {
-    this.getData()
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      cities: [],
+    };
   }
 
   getData() {
-    // create a new XMLHttpRequest
-    var xhr = new XMLHttpRequest()
-
-    // get a callback when the server responds
-    xhr.addEventListener('load', () => {
-      // update the state of the component with the result here
-      console.log(xhr.responseText)
-    })
-    // open the request with the verb and the url
-    xhr.open('GET', 'https://api.jcdecaux.com/vls/v1/contracts?apiKey='+ this.ApiKey)
-    // send the request
-    xhr.send()
+    fetch("https://api.jcdecaux.com/vls/v1/contracts?apiKey=" + this.ApiKey)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setState({ cities: data });
+      })
+      .catch((err) => console.error(this.props.url, err.toString()));
   }
 
+  async componentWillMount() {
+    this.getData();
+
+  }
   render() {
     return (
       <div>
-        <p>Hello World</p>
+        {this.state.cities.map((city,i) => (
+          <p>{city.name}</p>
+        ))}
       </div>
-    )
+    );
   }
 }
 
